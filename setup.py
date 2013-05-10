99569a319d06d327240abbcd2198dde547083a3a
Taito Horiuchi <taito.horiuchi@gmail.com>
2013-05-10 11:16:46 +0300

    Release 0.4

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='sll.portlet',
-    version='0.3',
+    version='0.4',
     description="Portlets for SLL Site.",
     long_description=open("README.rst").read(),
     classifiers=[
